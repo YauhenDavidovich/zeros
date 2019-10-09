18fd5cc75f49e35c65e7220a294312df573fe5a8
Пользователь <davidovich336@gmail.com>
2019-10-09 15:35:19 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function zeros(expression) {
+  const fact = 100;
+  let n = 0;
+  for (let i = 1; i <= fact ; i++) {
+    if (i % 25 === 0) {
+      n+= 2
+    } else if (i % 5 == 0) {
+        n++ 
+        } 
+    } 
+    
   // your solution
 }
