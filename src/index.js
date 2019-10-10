57d5e7cae0eae55164b4d892445b306da6bfa553
Евгений Давидович <davidovichyauhen@gmail.com>
2019-10-10 23:28:57 +0300

    feat: make double factorial analyzer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function zeros(expression) {
-  const fact = 100;
+  var fact = eval(expression.replace(/\*/g, '*').replace(/!/g, ""));
   let n = 0;
   for (let i = 1; i <= fact ; i++) {
     if (i % 25 === 0) {
@@ -8,6 +8,25 @@ module.exports = function zeros(expression) {
         n++ 
         } 
     } 
-    
+  return n; 
   // your solution
 }
+
+let t = '1!!*4!*7!!*2!!';
+
+var newStr = '', i;
+    for (i = t.length - 1; i >= 0; i--) {
+        newStr += t.charAt(i);
+    }
+let arr = newStr.split('*');
+let double_factorial = []
+let j=0;
+
+for (let i =0; i < arr.length; i++) { 
+  let small_arr = arr[j].split('');
+  if (small_arr[0] == small_arr[1]) {
+    double_factorial += arr[j].replace(/\!!/, '')    
+  } 
+  j++
+    }  
+  
